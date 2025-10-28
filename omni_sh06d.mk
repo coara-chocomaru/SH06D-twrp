55d21646d3ced50c31f6408d800321d8a256da35
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-29 03:58:24 +0900

    Update omni_sh06d.mk

--- omni_sh06d.mk
+++ omni_sh06d.mk
@@ -9,15 +9,15 @@
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from sh06d device
-$(call inherit-product, device/shrp/sh06d/device.mk)
+$(call inherit-product, device/sharp/sh06d/device.mk)
 
 PRODUCT_DEVICE := sh06d
 PRODUCT_NAME := omni_sh06d
-PRODUCT_BRAND := shrp
+PRODUCT_BRAND := sharp
 PRODUCT_MODEL := SH-06D
 PRODUCT_MANUFACTURER := SHARP
 
-PRODUCT_GMS_CLIENTID_BASE := android-shrp
+PRODUCT_GMS_CLIENTID_BASE := android-sharp
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="SH06D-user 4.0.4 S9090 02.00.04 release-keys"
